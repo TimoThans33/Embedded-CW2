8999aafaf7d9ee1321ca76356190d3992a7e2fd5
alexwillie <alex.willie.adolfsson@gmail.com>
2020-03-12 15:06:49 +0000

    messsage Update

---
+++ message.h
@@ -0,0 +1,22 @@
+#ifndef _message_h_
+#define _message_h_
+
+ #include "rtos.h"
+
+ // The output codes
+
+ #define START  0
+
+
+ extern RawSerial pc;
+
+ typedef struct {
+   uint8_t command;
+   int32_t data;
+ }message_struct
+
+ void getMail();
+
+ extern void setMail(uint8_t command, int32_t data);
+
+ #endif
